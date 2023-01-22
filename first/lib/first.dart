6d4f218d79a50dc661f4f4fbe0598248d2218008
akdl912115@palpals.co.kr <akdl911215@palpals.co.kr>
2023-01-22 12:41:42 +0900

    first commit

---
+++ first/lib/first.dart
@@ -0,0 +1,27 @@
+import 'package:css_colors/css_colors.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+
+    return MaterialApp(
+      home: DemoPage()
+    );
+  }
+}
+
+class DemoPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+
+    return Scaffold(body: Container(color: CSSColors.chocolate));
+  }
+
+}
+
+int calculate() {
+  return 6 * 7;
+}
