e48f1744647d22bcda1c790a66230b6755c25c83
akdl912115@palpals.co.kr <akdl911215@naver.com>
2023-01-23 21:07:54 +0900

    2d game sdt

--- game/example/lib/main.dart
+++ game/example/lib/main.dart
@@ -25,3 +25,5 @@ class SomeOneGameApp extends StatelessWidget {
   }
 
 }
+
+// https://rlg1133.tistory.com/140
--- game/lib/henry_some_one.dart
+++ game/lib/henry_some_one.dart
@@ -1,6 +1,10 @@
 import 'package:flame/game.dart';
+import 'package:flutter/material.dart';
 import 'package:game/animated_player.dart';
 import 'package:game/game_background.dart';
+import 'package:game/item.dart';
+import 'package:game/item_list_bottom_sheet.dart';
+import 'package:sliding_up_panel/sliding_up_panel.dart';
 import 'index.dart';
 
 class HenrySomeOneGame extends StatefulWidget {
@@ -11,15 +15,42 @@ class HenrySomeOneGame extends StatefulWidget {
 }
 
 class _HenrySomeOneGameState extends State {
+
+  List<int> list = [];
+  void onTapItem(int index) {
+    debugPrint(index.toString());
+    if(list.contains(index)) return;
+    list.add(index);
+    setState(() {});
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const GameWidget.controlled(
-        gameFactory: MyStaticGame.new
+    return SlidingUpPanel(
+      minHeight: 40,
+      borderRadius: const BorderRadius.vertical(top: Radius.circular(20)),
+      panel: ItemListBottomSheet(onTapItem: onTapItem ,),
+      body: GameWidget.controlled(
+        gameFactory: () => MyStaticGame(list: list),
+        loadingBuilder: (context) => const Material(
+          child: Center(
+            child: CircularProgressIndicator(),
+          ),
+        ),
+        errorBuilder: (context, ex) => const Material(
+          child: Center(
+            child: Text('Error!!!'),
+          ),
+        ),
+      ),
     );
   }
 }
 
 class MyStaticGame extends FlameGame with HasCollisionDetection {
+
+  List<int> list;
+  MyStaticGame({ required this.list });
 
   @override
   Color backgroundColor() => const Color(0x00000000);
@@ -35,5 +66,15 @@ class MyStaticGame extends FlameGame with HasCollisionDetection {
     final playerPosition = Vector2(200, 600);
     add(AnimatedPlayer(playerPosition, playerSize));
   }
+
+  @override
+  void update(double dt) {
+    super.update(dt);
+    if(list.isEmpty) return;
+    Vector2 position = Vector2(list.last * 50, 100);
+    add(Item(position, index: list.last));
+  }
+
+
 }
 
